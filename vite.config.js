abe569b30754c1a7db0ea1de3ccd016d8a83765f
Vaibhav Sharma <vssharma.vaibhav5@gmail.com>
2025-08-04 19:37:17 +0530

    change

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,30 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-// import ghPages from 'vite-plugin-gh-pages';
+import { visualizer } from 'rollup-plugin-visualizer'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/ai-tooltip/', 
+  plugins: [
+    react(),
+    // visualizer({
+    //   filename: 'bundle-stats.html',
+    //   open: true, // opens report in browser after build
+    //   gzipSize: true,
+    //   brotliSize: true,
+    // })
+  ],
+  base: '/ai-tooltip/',
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            const parts = id.toString().split('node_modules/')[1].split('/');
+            // Group all packages under their top-level scope
+            return parts[0].startsWith('@') ? `${parts[0]}/${parts[1]}` : parts[0];
+          }
+        }
+      }
+    },
+    chunkSizeWarningLimit: 1000 // Optional: increase warning threshold
+  }
 })
